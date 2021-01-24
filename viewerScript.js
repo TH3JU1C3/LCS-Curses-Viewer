64a1cd6ecc2c342f951f53407fb6d9dc49c54a38
TH3JU1C3 <74450373+TH3JU1C3@users.noreply.github.com>
2021-01-24 16:07:44 +0000

    Added Ability to change Frames
    Refactored code in viewerScript.js
    Added Buttons to change frame in html
    Made it so that only cmv and cpc files are accepted

--- viewerScript.js
+++ viewerScript.js
@@ -1,6 +1,12 @@
-document.getElementById("A").innerHTML = "Hello";
+let data = [];
+let frameNum = 0;
+const canvas = document.getElementById("movieScreen");
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+const ctx = canvas.getContext("2d");
 
 function readFile() {
+	data = [];
 	let files = document.getElementById('uploader').files;
 	console.log(files);
 	if (files.length <= 0) {
@@ -13,54 +19,68 @@ function readFile() {
 		let x = fr.result;
 		let str = "";
 		for (let i = 0; i < x.length; i++) {
-			str += x.charCodeAt(i) + " ";
+			data.push(x.charCodeAt(i));
 		}
-		console.log(str);
-		displayMovie(x);
+		console.log(data);
+		displayPicture();
+		displayFrameNum();
 	}
 };
 
-function displayMovie(data) {
-	const picnum = data.charCodeAt(0);
-	const dimx = data.charCodeAt(4);
-	const dimy = data.charCodeAt(8);
+function displayPicture() {
+	const picnum = data[0];
+	const dimx = data[4];
+	const dimy = data[8];
+	const pictureBlockSize = dimx*dimy;
 	
-	const canvas = document.getElementById("movieScreen");
-	const canvasWidth = canvas.width;
-	const canvasHeight = canvas.height;
-	const ctx = canvas.getContext("2d");
 	ctx.fillStyle = "rgba(0,0,0,1)";
 	ctx.fillRect(0,0,canvasWidth,canvasHeight);
 	ctx.font = "16px Consolas";
 	ctx.fillStyle = "white";
-	//ctx.fillText("Hi There",0,25);
-	//ctx.fillText("Mr Gay",0,50);
 	let colours = ["black","blue","green","cyan","red","magenta","yellow","white"];
 	let bColour = colours[0];
 	let fColour = colours[7];
-	let dword = 3;
+	let dword = 3 + (frameNum * pictureBlockSize);
 	let dword4 = dword*4;
 	let charac = "";
-	for (let x = 0; x < dimx; x++) {
-		for (let y = 0; y < dimy; y++) {
-				fColour = colours[data.charCodeAt(dword4+1)];
-				bColour = colours[data.charCodeAt(dword4+2)];
+	for (let x = 0; x < dimx && x < 80; x++) {
+		for (let y = 0; y < dimy && y < 25; y++) {
+				fColour = colours[data[dword4+1]];
+				bColour = colours[data[dword4+2]];
 				ctx.fillStyle = bColour;
 				ctx.fillRect(x*8,y*16,(x+1)*8,(y+1)*16);
 				ctx.fillStyle = fColour;
-				charac = data.charAt(dword4);
+				charac = String.fromCharCode(data[dword4]);
 				dword++;
 				dword4 = dword*4;
 				if (charac == "\0" || charac == " ") {
 					continue;
 				}
 				ctx.fillText(charac,x*8,y*16);
-				console.log(data.charCodeAt(dword4),data.charCodeAt(dword4 + 1),data.charCodeAt(dword4 + 2),data.charCodeAt(dword4 + 3));
-				if (dword > 5000) {
-					console.log("XXX");
-				}
-				//cCount = (cCount + 1) % colours.length;
+				console.log(data[dword4],data[dword4 + 1],data[dword4 + 2],data[dword4 + 3]);
 		}
 	}
 	console.log("G");
+}
+
+function incrementFrameNumAndDisplay() {
+	frameNum++;
+	if (frameNum > data[0]) {
+		frameNum = 0;
+	}
+	displayPicture();
+	displayFrameNum();
+}
+
+function decrementFrameNumAndDisplay() {
+	frameNum--;
+	if (frameNum < 0) {
+		frameNum = data[0];
+	}
+	displayPicture();
+	displayFrameNum();
+}
+
+function displayFrameNum() {
+	document.getElementById("FrameDisplayer").innerHTML = "Current Frame: " + frameNum;
 }
